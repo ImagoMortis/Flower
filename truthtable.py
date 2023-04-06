012ed9b55bf7d4beca67d3311a7bce6eeb8336d0
Manuel <114886382+ImagoMortis@users.noreply.github.com>
2023-04-06 10:34:41 +0200

    Update truthtable.py

--- truthtable.py
+++ truthtable.py
@@ -3,8 +3,6 @@ print("---------")
 for a in range(2):
 	for b in range(2):
 		for c in range(2):
-			# die naechste Codezeile enthaelt die Formel aus der VL;
-			# bitte durch eigene Formel ersetzen!
 			y = not((a==1) and (b==1)) and (c==1)
 			r = int(y)
 			print(a,b,c,"|",r)
